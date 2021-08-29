7d6575af56daa79a91a15b52a3cb5ccabd7e5718
Veronika <veronika.ria.v@gmail.com>
2021-08-29 19:24:17 +0300

    Add timer taghelper

---
+++ RegistrationForm/TagHelpers/TimerTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace RegistrationForm.TagHelpers
+{
+    public class TimerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "timer";
+            output.Content.SetContent(DateTime.Now.ToString());
+        }
+    }
+}
